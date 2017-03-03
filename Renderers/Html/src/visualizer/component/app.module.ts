117fbd02380c35ea599b316601ceb6e9d8cf9828
Vishnuvardhan Vaidhyanathan <vivai@microsoft.com>
2017-03-02 18:42:10 -0800

    Add component

--- Renderers/Html/src/visualizer/component/app.module.ts
+++ Renderers/Html/src/visualizer/component/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'; 
 import { BrowserModule } from '@angular/platform-browser'; 
 import { AdaptiveCardComponent } from './app.component'; 
+import { CardRendererComponent } from './card-renderer.component';
 
 @NgModule({ 
   imports: [BrowserModule], 
-  declarations: [AdaptiveCardComponent], 
+  declarations: [AdaptiveCardComponent, CardRendererComponent], 
   bootstrap: [AdaptiveCardComponent] 
 }) 
 
---
+++ Renderers/Html/src/visualizer/component/card-renderer.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit, AfterViewInit} from '@angular/core';
+import {HostApp} from '../App';
+
+@Component({
+  selector: 'card-renderer',
+  templateUrl: './component/card-renderer.html'
+})
+export class CardRendererComponent implements AfterViewInit {
+  constructor() {}
+
+  ngAfterViewInit() {
+  }
+}
