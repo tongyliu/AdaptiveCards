f34d6123087d3212aca9f439bd6dfc94f958cc72
Vishnuvardhan Vaidhyanathan <vivai@microsoft.com>
2017-03-06 18:15:23 -0800

    Add header component

--- Renderers/Html/src/visualizer/component/app.module.ts
+++ Renderers/Html/src/visualizer/component/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; 
 import { AdaptiveCardComponent } from './app.component'; 
 import { CardRendererComponent } from './card-renderer.component';
+import { CardHeaderComponent } from './card-header.component';
 
 @NgModule({ 
   imports: [BrowserModule], 
-  declarations: [AdaptiveCardComponent, CardRendererComponent], 
+  declarations: [AdaptiveCardComponent, CardRendererComponent, CardHeaderComponent], 
   bootstrap: [AdaptiveCardComponent] 
 }) 
 
---
+++ Renderers/Html/src/visualizer/component/card-header.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit, AfterViewInit, 
+        Input, Output, EventEmitter, OnDestroy,
+        ChangeDetectionStrategy, ChangeDetectorRef
+} from '@angular/core';
+import {HostApp} from '../App';
+import {DataTransferService} from './DataTransferService';
+import {Subscription} from 'rxjs/Rx';
+
+@Component({
+  selector: 'card-header',
+  templateUrl: './component/card-header.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CardHeaderComponent implements AfterViewInit, OnDestroy {
+
+    constructor(){}
+
+    ngAfterViewInit() {
+        
+    }
+
+    ngOnDestroy(){
+
+    }
+}
